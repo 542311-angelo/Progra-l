4311c15aa0fea153a2ae3a949a702e35d2030fba
542311-angelo <79349347+542311-angelo@users.noreply.github.com>
2021-03-05 17:35:42 -0600

    Add files via upload

---
+++ parcial progra/adnum/adnum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace adnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, resultado;
+
+            Console.WriteLine("Piense en un numero, y no lo comente con nadie");
+            Console.ReadKey();
+            Console.WriteLine("Multiplicar por 2");
+            Console.ReadKey();
+            Console.WriteLine("Sumele 8");
+            Console.ReadKey();
+            Console.WriteLine("Multiplicalo por 5");
+            Console.ReadKey();
+
+            Console.Write("Ingrese el resultado: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            numero = numero / 10;
+            resultado = numero - 4;
+
+            Console.WriteLine("El numero que usted penso es: " + resultado);
+            Console.WriteLine("gracias por jugar con nosiotros");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ parcial progra/dia_mes/dia_mes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace dia_mes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = 0, m = 0, resultado;
+
+            Console.WriteLine("Ingrese su dia de nacimiento");
+            dia = Convert.ToInt32(Console.ReadLine());
+            dia = ((dia * 20) + 73) * 5;
+            Console.WriteLine("ingrese el numero de su mes de nacimiento: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            resultado = dia + m - 365;
+            int can1, can2, can3, can4;
+            can1 = (resultado % 10000) / 1000;
+            can2 = (resultado % 10000) % 1000 / 100;
+            can3 = ((resultado % 10000) % 1000) % 100 / 10;
+            can4 = (((resultado % 10000) % 1000) % 100) % 10;
+            Console.WriteLine("Tunaciste el dia: " + can1 + can2 + " del mes:" + can3 + can4);
+        }
+    }
+}
